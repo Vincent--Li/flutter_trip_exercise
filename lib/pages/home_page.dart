d623580dd6769d18831bbf66ea38d6e41840d65e
Vincent <315744068@qq.com>
2020-01-06 11:28:48 +0800

    UPDATE: 搜索组件添加

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,93 +3,58 @@ import 'package:flutter_app/dao/home_dao.dart';
 import 'package:flutter_app/model/common_model.dart';
 import 'package:flutter_app/model/grid_nav_model.dart';
 import 'package:flutter_app/model/home_model.dart';
+import 'package:flutter_app/model/sales_box_model.dart';
 import 'package:flutter_app/widget/grid_nav.dart';
+import 'package:flutter_app/widget/loading_container.dart';
 import 'package:flutter_app/widget/local_nav.dart';
+import 'package:flutter_app/widget/sales_box.dart';
+import 'package:flutter_app/widget/search_bar.dart';
+import 'package:flutter_app/widget/sub_nav.dart';
+import 'package:flutter_app/widget/webview.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
 
 const APPBAR_SCROLL_OFFSET = 100;
+const SEARCH_BAR_DEFAULT_TEXT = '网红打卡地 景点 酒店 美食';
 class HomePage extends StatefulWidget{
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage>{
-  List _imageUrls = [
-    'https://dimg04.c-ctrip.com/images/3005190000015mw1n7799_C_200_150.jpg',
-    'https://dimg03.c-ctrip.com/images/fd/tg/g2/M0A/89/ED/CghzgFWwxoaAB2jnABsQ9aZOizI872_C_200_150.jpg',
-    'https://dimg04.c-ctrip.com/images/300b0v000000k60bdA235_C_200_150.jpg'
-  ];
 
   double appBarAlpha = 0;
   List<CommonModel> localNavList = [];
+  List<CommonModel> subNavList = [];
+  List<CommonModel> bannerList = [];
   GridNavModel gridNavModel;
+  SalesBoxModel salesBoxModel;
+  bool _loading = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xfff2f2f2 ),
-      body: Stack(
+      backgroundColor: Color(0xfff2f2f2),
+      body: LoadingContainer(isLoading: _loading, child: Stack(
         children: <Widget>[
           MediaQuery.removePadding(removeTop: true, context: context,
-              child: NotificationListener(
-                onNotification: (scrollNotification){
-                  if(scrollNotification is ScrollUpdateNotification && scrollNotification.depth == 0){
-                    //滚动且是列表滚动的时候
-                    _onScroll(scrollNotification.metrics.pixels);
-                  }
-                  return true;
-                },
-                child: ListView(
-
-                  children: <Widget>[
-                    Container(
-                      height: 160,
-                      child: Swiper(
-                        itemCount: _imageUrls.length,
-                        autoplay: true,
-                        itemBuilder: ( BuildContext context, int index){
-                          return Image.network(
-                              _imageUrls[index],
-                              fit: BoxFit.fill
-                          );
-                        },
-                        pagination: SwiperPagination(),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
-                      child:  LocalNav(localNavList: localNavList,),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
-                      child: GridNav(gridNavModel: gridNavModel,),
-                    ),
-                    Container(
-                      height: 800,
-                      child: ListTile(
-                        title: Text('test'),
-                      ),
-                    ),
-                  ],
+              child: RefreshIndicator(
+                onRefresh: _handleRefresh,
+                child: NotificationListener(
+                  onNotification: (scrollNotification){
+                    if(scrollNotification is ScrollUpdateNotification && scrollNotification.depth == 0){
+                      //滚动且是列表滚动的时候
+                      _onScroll(scrollNotification.metrics.pixels);
+                    }
+                    return true;
+                  },
+                  child: _listView,
                 ),
               )
           ),
-          Opacity(
-            opacity: appBarAlpha,
-            child: Container(
-              height: 80,
-              decoration: BoxDecoration(color: Colors.white),
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 20),
-                  child: Text('首页'),
-                ),
-              ),
-            ),
-          )
+          _appBar,
         ],
-      )
+      ),)
     );
   }
 
@@ -110,7 +75,7 @@ class _HomePageState extends State<HomePage>{
   @override
   void initState() {
     super.initState();
-    loadData();
+    _handleRefresh();
   }
 
 //  loadData() {
@@ -124,17 +89,127 @@ class _HomePageState extends State<HomePage>{
 //      });
 //    });
 //  }
-  loadData() async {
+
+  Widget get _listView {
+    return ListView(
+
+      children: <Widget>[
+        _banner,
+        Padding(
+          padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
+          child:  LocalNav(localNavList: localNavList,),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
+          child: GridNav(gridNavModel: gridNavModel,),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
+          child: SubNav(subNavList: subNavList,),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
+          child: SalesBox(salesBox: salesBoxModel,),
+        ),
+      ],
+    );
+  }
+
+  Widget get _appBar {
+    return Column(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0x66000000), Colors.transparent],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            )
+          ),
+          child: Container(
+            padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+            height: 80.0,
+            decoration: BoxDecoration(
+              color: Color.fromARGB((appBarAlpha * 255).toInt(), 255, 255, 255)
+            ),
+            child: SearchBar(
+              searchBarType: appBarAlpha > 0.2 ? SearchBarType.homeLight : SearchBarType.home,
+              inputBoxClick: _jumpToSearch,
+              speackClick: _jumpToSpeak,
+              defaultText: SEARCH_BAR_DEFAULT_TEXT,
+              leftButtonClick: () {},
+            ),
+          ),
+        ),
+        Container(
+          height: appBarAlpha > 0.2 ? 0.5 : 0,
+          decoration: BoxDecoration(
+            boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 0.5)]
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget get _banner {
+    return Container(
+      height: 160,
+      child: Swiper(
+        itemCount: bannerList.length,
+        autoplay: true,
+        itemBuilder: ( BuildContext context, int index){
+          return GestureDetector(
+            child: Image.network(
+                bannerList[index].icon,
+                fit: BoxFit.fill
+            ),
+            onTap: (){
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context){
+                    CommonModel model = bannerList[index];
+                    return WebView(
+                      url: model.url,
+                      statusBarColor: model.statusBarColor,
+                      hideAppBar: model.hideAppBar,);
+                  }
+                  )
+              );
+            },
+          );
+        },
+        pagination: SwiperPagination(),
+      ),
+    );
+  }
+
+  Future<Null> _handleRefresh() async {
     try{
       HomeModel model = await HomeDao.fetch();
       setState(() {
         localNavList = model.localNavList;
         gridNavModel = model.gridNav;
+        subNavList = model.subNavList;
+        salesBoxModel = model.salesBox;
+        bannerList = model.bannerList;
+        _loading = false;
       });
     }catch(e){
       print(e);
+      setState(() {
+        _loading = false;
+      });
     }
+    return null;
   }
 
 
+
+  _jumpToSearch() {
+
+  }
+
+  _jumpToSpeak(){
+
+  }
 }
--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/widget/search_bar.dart';
 
 class SearchPage extends StatefulWidget{
   @override
@@ -10,10 +11,27 @@ class _SearchPageState extends State<SearchPage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('搜索'),
-      ),
+      body: Column(
+        children: <Widget>[
+          Text(
+            '临时把搜索框顶下去',
+            style: TextStyle(color: Colors.blue, fontSize: 20 ),
+          ),
+          SearchBar(
+            hideLeft: true,
+            defaultText: 'xixi',
+            hint: '123',
+            leftButtonClick: (){
+              Navigator.pop(context);
+            },
+            onChanged: _onTextChange,
+          )
+        ],
+      )
     );
   }
 
+
+  _onTextChange(String text) {
+  }
 }
